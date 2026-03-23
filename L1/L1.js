3e6529de9c54358182634470afdc26431f3b3164
S <sabatabatadze01101001@gmail.com>
2026-03-23 07:43:37 +0400

    a

---
+++ L1/L1.js
@@ -0,0 +1,23 @@
+//1.
+
+function first(t){
+    document.writeln(`<p style='font-style:italic; text-decoration:underline; font-weight bold'>${t}</p>1`)
+}
+
+//2.
+
+function second(a,b){
+    return a+b;
+}
+
+//3 
+
+function third(t, s){
+    document.writeln(`<p style:'font size=${s}px' ${t} </p>`)
+}
+
+//4.
+
+function fourth(w){
+    document.writeln(``)
+}
