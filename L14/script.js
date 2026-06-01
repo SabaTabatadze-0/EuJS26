c8c6eac91f42692c26e80eff081f7c3b335062fa
European Student <student@eu.local>
2026-06-01 11:49:56 +0400

    L14C2

--- L14/script.js
+++ L14/script.js
@@ -1,5 +1,5 @@
 function rndmNumGen(){
-    return Math.round(Math.random() * 750)
+    return Math.random() * 750
 }
 
 function genBall(){
@@ -7,6 +7,12 @@ function genBall(){
     ball.classList.add("ball")
     ball.style.top = rndmNumGen()+"px"
     ball.style.left = rndmNumGen()+"px"
+    var num = Math.floor(Math.random() * 10)
+    var r = Math.random() * 255
+    var g = Math.random() * 255
+    var b = Math.random() * 255
+    ball.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    ball.innerText = num
     var sq = document.querySelector("#field")
     sq.append(ball)
 }
